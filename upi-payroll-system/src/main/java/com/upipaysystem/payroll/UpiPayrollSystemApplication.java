827a50f2a6e5702ab9d59b0babced9d5b1828373
ayushman1626 <ayushmanpal1626@gmail.com>
2025-04-17 22:49:15 +0530

    Initial commit - UPI Payroll System base structure

---
+++ upi-payroll-system/src/main/java/com/upipaysystem/payroll/UpiPayrollSystemApplication.java
@@ -0,0 +1,13 @@
+package com.upipaysystem.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiPayrollSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiPayrollSystemApplication.class, args);
+	}
+
+}
---
+++ upi-payroll-system/src/test/java/com/upipaysystem/payroll/UpiPayrollSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upipaysystem.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiPayrollSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
